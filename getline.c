029d24dc6dfe35669cd27d9f56d31f93db062358
FisayoOladejo <oladejofisayo1@gmail.com>
2022-11-16 08:51:38 -0800

    newsimple

--- getline.c
+++ getline.c
@@ -5,7 +5,7 @@
  * @info: parameter struct
  * @buf: address of buffer
  * @len: address of len var
- *
+ * collaboration
  * Return: bytes read
  */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
