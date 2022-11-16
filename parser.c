3ee30a51f2bc38339c31779f110ad705a0e6b414
FisayoOladejo <oladejofisayo1@gmail.com>
2022-11-16 08:47:43 -0800

    newsimple

--- parser.c
+++ parser.c
@@ -2,7 +2,7 @@
 
 /**
  * is_cmd - determines if a file is an executable command
- * @info: the info struct good
+ * @info: the info struct 
  * @path: path to the file
  *
  * Return: 1 if true, 0 otherwise
